dbba9f022acde234e9b76ec2cb87fb5bb2a35015
saeed <test@gmail.com>
2025-11-22 10:49:50 +0330

    fix wrongs

--- to_do_list-app/frontend/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-// @ts-ignore
-import tailwindcss from '@tailwindcss/vite'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss(),],
-})

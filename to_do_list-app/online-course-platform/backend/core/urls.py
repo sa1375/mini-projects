bc7aa03a191a541826536566e26e8f02e4b579a3
saeed <test@gmail.com>
2025-11-22 10:42:54 +0330

    fix errors

--- to_do_list-app/online-course-platform/backend/core/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import test_api
-
-urlpatterns = [
-    path('test/', test_api, name='test_api'),
-]
--- to_do_list-app/online-course-platform/backend/core/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import JsonResponse
-
-
-# Create your views here.
-def test_api(request):
-    return JsonResponse({"message": "Hello from Django"})
